9b6c23edd553db148eb4a5f92b144f156e0fd47d
Jihye <wlgul817@gmail.com>
2019-04-14 16:13:23 +0900

    branch commit test

--- Register.js
+++ Register.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 import {StyleSheet} from 'react-native';
 import cstyle from './Styles';
-
+//test branch commit test
 export default class Mainpage extends React.Component{
     render() {
         return (
