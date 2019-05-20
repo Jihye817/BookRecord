f4e42a2d1c103035ebfda69267facc7e261b0cc5
SunHabin <sunho8692@naver.com>
2019-05-20 17:09:50 +0900

    Update Found.js

--- Found.js
+++ Found.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, SafeAreaView, StatusBar,TextInput, style} from 'react-native';
+import {View, Text, StyleSheet, SafeAreaView, StatusBar,TextInput, TouchableHighlight, ScrollView} from 'react-native';
 import cstyle from './Styles';
+import { TouchableHighlight, ScrollView } from 'react-native-gesture-handler';
+//import console = require('console');
 
 export default class Found extends React.Component{
     constructor(props){
@@ -9,11 +11,37 @@ export default class Found extends React.Component{
             apiData: []
         }
         this.dataId = null;
-        this.name = null;
+        this.user_name = null;
         this.email = null;
         this.phone_number = null;
     }
+
+    getButton = () => {
+        fetch('http://220.149.242.12:3000/user',{
+            method: 'GET'
+        }).then((response) => {
+            return responseData.json();
+        }).them((jsonData) => {
+            console.log(jsonData);
+            this.setState({apiData:jsonData})
+            console.log(this.state.apiData)
+        }).done();
+        this.dataId = null;
+    }
+
     render(){
+        const data = this.state.apiData;
+        let dataDisplay = data.map(function(jsonData){
+            return (
+                <View key={jsonData.id}>
+                    <View style={{flexDirection:'row'}}>
+                        <Text style={{ color:'#000'}}>{jsonData.id}</Text>
+                        <Text style={{ color:'#000'}}>{jsonData.user_name}</Text>
+                        <Text style={{ color:'#000'}}>{jsonData.email}</Text>
+                    </View>
+                </View>
+            )
+        });
         return(
             <SafeAreaView style = {[cstyle.greencontainer, {flex : 1}]}>
                 <StatusBar hidden />
@@ -23,20 +51,32 @@ export default class Found extends React.Component{
                     </View>
                     <View style = {styles.infocontainer}>
                         <Text>App users</Text>
-                    </View>
-
-                    <TextInput style={style.input}
+                        <TextInput style={styles.input}
                         placeholder = 'Id'
                         onChangeText = {(text) => {this.dataId = text}}
                         value = {this.dataId}
                         underlineColorAndroid = 'transparent'
-                    />
-                    <TextInput style={style.input}
+                        />
+                        <TextInput style={styles.input}
                         placeholder = 'Enter Name'
-                        onChangeText = {(text) => {this.name = text}}
-                        value = {this.name}
+                        onChangeText = {(text) => {this.user_name = text}}
+                        value = {this.user_name}
                         underlineColorAndroid = 'transparent'
-                    />
+                        />  
+                        <TextInput style={styles.input}
+                        placeholder = 'Enter email'
+                        onChangeText = {(text) => {this.email = text}}
+                        value = {this.email}
+                        underlineColorAndroid = 'transparent'
+                        />
+                        <TouchableHighlight style={styles.button} onPress = {this.getButton}>
+                            <Text style = {styles.textStyle}>View Data</Text>
+                        </TouchableHighlight>
+
+                        <ScrollView contentContainerStyle={styles.container}>
+                            {dataDisplay}
+                        </ScrollView>
+                    </View>
                 </View>
             </SafeAreaView>
         );
@@ -65,7 +105,20 @@ const styles = StyleSheet.create({
         width : '90%',
         padding : 4,
         marginBottom : 7,
-        fontSize:34,
+        fontSize : 14,
         borderWidth : 1,
+    },
+    button: {
+        paddingTop:10,
+        paddingBottom : 10,
+        borderRadius : 5,
+        marginBottom: 3,
+        width : '90%',
+        backgroundColor : '#00BCD4'
+    },
+    textStyle: {
+        color: '#fff',
+        fontSize : 14,
+        textAlign: 'center'
     }
 });
