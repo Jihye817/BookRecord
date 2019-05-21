ff29dcf372dc1d18dfd0942d4399df465365ebda
SunHabin <sunho8692@naver.com>
2019-05-21 21:48:00 +0900

    ISBNsearch.js(first)

--- Isbnsearch.js
+++ Isbnsearch.js
@@ -1,86 +1,132 @@
-import React, {Component} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, TextInput, Image, Picker} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, TextInput, Image, Picker } from 'react-native';
 import cstyle from './Styles';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import Modal from 'react-native-modal';
 import SwitchButton from 'switch-button-react-native';
+//import console = require('console');
 
-export default class Isbnsearch extends React.Component{
+export default class Isbnsearch extends React.Component {
+    //constructor = 생성자 = 클래스가 생성될 때 제일 먼저 실행되는 함수
+    // this.state={} state가 사용할 변수를 초기화
+    // 밑의 함수에서 this.setState() 함수를 사용하여 update
     constructor(props) {
         super(props);
         this.state = {
             isPopVisible: false,
-        };
+            apiData: [
+                this.ISBN = null,
+                this.book_name = null,
+                this.img_src = null,
+                this.author = null,
+                this.publisher = null,
+                this.public_date = null,
+                this.more_url = null,
+                this.read_rate = null,
+                this.read_date = null,
+                this.category = null,
+                this.best = null
+            ]
+        }
     }
 
     togglePop = () => {
-        fetch('http://220.149.242.12:10001/search/book',{
+        fetch('http://220.149.242.12:10001/search/book/:'+(this.ISBN), {
             method: 'GET'
         }).then((responseData) => {
             return responseData.json();
-        })
-        this.setState({ isPopVisible : !this.state.isPopVisible});
+        }).them((jsonData) => {
+            console.log(jsonData);
+            this.setState({ apiData: jsonData })
+            console.log(this.state.apiData)
+        }).done();
+        this.setState({ isPopVisible: !this.state.isPopVisible });
+        this.ISBN = null;
+        this.book_name = null;
+        this.img_src = null;
+        this.author = null;
+        this.publisher = null;
+        this.public_date = null;
+        this.more_url = null;
+        this.read_rate = null;
+        this.read_date = null;
+        this.category = null;
+        this.best = null;
     }
 
-    render(){
-        return(
-            <View style = {cstyle.greycontainer}>
-                <View style = {styles.firstbox}>
-                    <Text style = {{color: '#FFF', fontSize: 20}}>책의 ISBN을 입력하세요</Text>
+    render() {
+        const data = this.state.apiData;
+        let dataDisplay = data.map(function (jsonData) {
+            this.read_date = getFullYear() + getMonth() + getDate();
+            console.log(this.read_date);
+            return (
+                <View style={styles.popthird}>
+                    <View style={{ paddingTop: 30, }}>
+                        <Text style={{ color: '#52C8B2', fontSize: 20, }}>도서 정보 확인</Text>
+                    </View>
+                    <View style={{ paddingTop: 20, }}>
+                        <Image  style={{ width: 150, resizeMode: 'contain', }} 
+                                source={{uri:jsonData.image}}>
+                        </Image>
+                    </View>
+                    <View style={{ paddingTop: 10, }}>
+                        <Text style={{ fontSize: 18, }}>{jsonData.title}</Text>
+                    </View>
+                    <View style={{ paddingTop: 10, }}>
+                        <Text style={{ color: '#D7D7D7' }}>{jsonData.author} | {jsonData.publisher} | {this.read_date}</Text>
+                    </View>
+                    <View style={styles.popbtn}>
+                        <View style={{ width: 10, }}></View>
+                        <View style={styles.popbtnleft}>
+                            <SwitchButton
+                                onValueChange={(val) => this.setState({ activeSwitch: val })}
+                                text1='읽는 중'
+                                text2='완독'
+                                switchWidth={120}
+                                switchHeight={30}
+                                switchdirection='ltr'
+                                switchBorderRadius={0}
+                                switchSpeedChange={500}
+                                switchBorderColor='#52C8B2'
+                                switchBackgroundColor='#F2F2F2'
+                                btnBorderColor='#52C8B2'
+                                btnBackgroundColor='#52C8B2'
+                                fontcolor='#333'
+                                activeFontColor='#FFF'
+                            />
+                        </View>
+                    </View>
+                    <View style={styles.popbtnbig}>
+                        <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>취소</Text></TouchableOpacity>
+                        <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>등록하기</Text></TouchableOpacity>
+                    </View>
                 </View>
-                <View style = {styles.secondbox}>
-                    <TextInput style = {styles.input} />
-                    <TouchableOpacity style = {styles.searchbtn} onPress = {this.togglePop}>
-                        <IonIcon name = "ios-search" size={30} color='#FFF' />
+            )
+        })
+        return (
+            <View style={cstyle.greycontainer}>
+                <View style={styles.firstbox}>
+                    <Text style={{ color: '#FFF', fontSize: 20 }}>책의 ISBN을 입력하세요</Text>
+                </View>
+                <View style={styles.secondbox}>
+                    <TextInput style={styles.input} 
+                        placeholder = "Enter ISBN"
+                        onChangeText = {(text) => {this.ISBN = text}}
+                        value = {this.ISBN}
+                    />
+                    <TouchableOpacity style={styles.searchbtn} onPress={this.togglePop}>
+                        <IonIcon name="ios-search" size={30} color='#FFF' />
                     </TouchableOpacity>
                 </View>
-                <View style = {styles.firstbox}>
-                    <TouchableOpacity style = {styles.greenbtn}>
-                        <Text style = {{color: '#FFF', fontSize: 20}}>등록 취소</Text>
+                <View style={styles.firstbox}>
+                    <TouchableOpacity style={styles.greenbtn}>
+                        <Text style={{ color: '#FFF', fontSize: 20 }}>등록 취소</Text>
                     </TouchableOpacity>
                 </View>
-                <Modal isVisible = {this.state.isPopVisible}>
-                    <View style ={styles.popfirst}>
-                        <View style = {styles.popsecond}>
-                            <View style ={styles.popthird}>
-                                <View style = {{ paddingTop:30,}}>
-                                    <Text style = {{color: '#52C8B2', fontSize: 20,}}>도서 정보 확인</Text>
-                                </View>
-                                <View style = {{ paddingTop:20,}}>
-                                    <Image style = {{width:150, resizeMode: 'contain',}} source={require('./images/for_i.jpg')}></Image>
-                                </View>
-                                <View style = {{ paddingTop:10,}}>
-                                    <Text style = {{fontSize: 18,}}>i에게</Text>
-                                </View>
-                                <View style = {{ paddingTop:10,}}>
-                                    <Text style = {{color: '#D7D7D7'}}>김소연 | 아침달 시집 | 2018-09-10</Text>
-                                </View>
-                                <View style = {styles.popbtn}>
-                                    <View style = {{width: 10,}}></View>
-                                    <View style = {styles.popbtnleft}>
-                                        <SwitchButton
-                                            onValueChange={(val) => this.setState({activeSwitch: val})}
-                                            text1 = '읽는 중'
-                                            text2 = '완독'
-                                            switchWidth = {120}
-                                            switchHeight = {30}
-                                            switchdirection = 'ltr'
-                                            switchBorderRadius = {0}
-                                            switchSpeedChange = {500}
-                                            switchBorderColor = '#52C8B2'
-                                            switchBackgroundColor = '#F2F2F2'
-                                            btnBorderColor = '#52C8B2'
-                                            btnBackgroundColor = '#52C8B2'
-                                            fontcolor = '#333'
-                                            activeFontColor = '#FFF'
-                                        />
-                                    </View>
-                                </View>
-                                <View style = {styles.popbtnbig}>
-                                    <TouchableOpacity style = {styles.bigbtn} onPress = {this.togglePop}><Text style = {{fontSize: 16, color: '#FFF'}}>취소</Text></TouchableOpacity>
-                                    <TouchableOpacity style = {styles.bigbtn} onPress = {this.togglePop}><Text style = {{fontSize: 16, color: '#FFF'}}>등록하기</Text></TouchableOpacity>
-                                </View>
-                            </View>
+                <Modal isVisible={this.state.isPopVisible}>
+                    <View style={styles.popfirst}>
+                        <View style={styles.popsecond}>
+                            {dataDisplay}
                         </View>
                     </View>
                 </Modal>
@@ -100,9 +146,9 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    input:{
+    input: {
         height: 40,
-        width:'70%',
+        width: '70%',
         backgroundColor: '#FFF'
     },
     searchbtn: {
@@ -125,50 +171,50 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     popfirst: {
-        flex:1, 
-        justifyContent: 'center', 
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     popsecond: {
-        backgroundColor: '#FFF', 
-        height: '80%', 
-        width: '90%', 
+        backgroundColor: '#FFF',
+        height: '80%',
+        width: '90%',
         alignItems: 'center'
     },
     popthird: {
-        width:'90%', 
-        justifyContent: 'center', 
+        width: '90%',
+        justifyContent: 'center',
         alignItems: 'center'
     },
     popbtn: {
-        width: '90%', 
-        flexDirection: 'row', 
-        paddingTop:15, 
+        width: '90%',
+        flexDirection: 'row',
+        paddingTop: 15,
         justifyContent: 'space-between'
     },
     popbtnleft: {
-        flexDirection: 'row', 
-        justifyContent: 'space-between', 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
         width: 120
     },
     smallbtn: {
-        width: 50, 
-        height: 26, 
-        backgroundColor: '#F2F2F2', 
-        justifyContent: 'center', 
+        width: 50,
+        height: 26,
+        backgroundColor: '#F2F2F2',
+        justifyContent: 'center',
         alignItems: 'center'
     },
     popbtnbig: {
-        width: '100%', 
-        flexDirection: 'row', 
-        justifyContent: 'space-around', 
-        paddingTop:10,
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingTop: 10,
     },
     bigbtn: {
-        width: 120, 
-        height: 35, 
-        backgroundColor: '#52C8B2', 
-        justifyContent: 'center', 
+        width: 120,
+        height: 35,
+        backgroundColor: '#52C8B2',
+        justifyContent: 'center',
         alignItems: 'center'
     },
 
