0362d6b9f0232568359554843388d67d4f74d669
SunHabin <sunho8692@naver.com>
2019-05-22 01:10:27 +0900

    Update Isbnsearch.js

--- Isbnsearch.js
+++ Isbnsearch.js
@@ -34,7 +34,7 @@ export default class Isbnsearch extends React.Component {
     }
 
     togglePop = () => {
-        fetch('http://220.149.242.12:10001/search/book/'+(this.ISBN), {
+        fetch('http://220.149.242.12:10001/search/book/' + (this.ISBN), {
             method: 'GET'
         }).then((responseData) => {
             return responseData.json();
@@ -60,48 +60,51 @@ export default class Isbnsearch extends React.Component {
     render() {
         const data = this.state.apiData;
         const today = this.state.currentDate;
-        let dataDisplay = function(jsonData){
-            alert("help");
+        let dataDisplay = function (jsonData) {
             return (
-                <View style={styles.popthird}>
-                    <View style={{ paddingTop: 30, }}>
-                        <Text style={{ color: '#52C8B2', fontSize: 20, }}>도서 정보 확인</Text>
-                    </View>
-                    <View style={{ paddingTop: 20, }}>
-                        <Image  style={{ width: 150, resizeMode: 'contain', }} 
-                                source={{uri:jsonData.items.image}}>
-                        </Image>
-                    </View>
-                    <View style={{ paddingTop: 10, }}>
-                        <Text style={{ fontSize: 18, }}>{jsonData.items.title}</Text>
-                    </View>
-                    <View style={{ paddingTop: 10, }}>
-                        <Text style={{ color: '#D7D7D7' }}>{jsonData.items.author} | {jsonData.items.publisher} | {today}</Text>
-                    </View>
-                    <View style={styles.popbtn}>
-                        <View style={{ width: 10, }}></View>
-                        <View style={styles.popbtnleft}>
-                            <SwitchButton
-                                onValueChange={(val) => this.setState({ activeSwitch: val })}
-                                text1='읽는 중'
-                                text2='완독'
-                                switchWidth={120}
-                                switchHeight={30}
-                                switchdirection='ltr'
-                                switchBorderRadius={0}
-                                switchSpeedChange={500}
-                                switchBorderColor='#52C8B2'
-                                switchBackgroundColor='#F2F2F2'
-                                btnBorderColor='#52C8B2'
-                                btnBackgroundColor='#52C8B2'
-                                fontcolor='#333'
-                                activeFontColor='#FFF'
-                            />
+                <View style={styles.popfirst}>
+                    <View style={styles.popsecond}>
+                        <View style={styles.popthird}>
+                            <View style={{ paddingTop: 30, }}>
+                                <Text style={{ color: '#52C8B2', fontSize: 20, }}>도서 정보 확인</Text>
+                            </View>
+                            <View style={{ paddingTop: 20, }}>
+                                <Image style={{ width: 150, resizeMode: 'contain', }}
+                                    source={{ uri: jsonData.items.image }}>
+                                </Image>
+                            </View>
+                            <View style={{ paddingTop: 10, }}>
+                                <Text style={{ fontSize: 18, }}>{jsonData.items.title}</Text>
+                            </View>
+                            <View style={{ paddingTop: 10, }}>
+                                <Text style={{ color: '#D7D7D7' }}>{jsonData.items.author} | {jsonData.items.publisher} | {today}</Text>
+                            </View>
+                            <View style={styles.popbtn}>
+                                <View style={{ width: 10, }}></View>
+                                <View style={styles.popbtnleft}>
+                                    <SwitchButton
+                                        onValueChange={(val) => this.setState({ activeSwitch: val })}
+                                        text1='읽는 중'
+                                        text2='완독'
+                                        switchWidth={120}
+                                        switchHeight={30}
+                                        switchdirection='ltr'
+                                        switchBorderRadius={0}
+                                        switchSpeedChange={500}
+                                        switchBorderColor='#52C8B2'
+                                        switchBackgroundColor='#F2F2F2'
+                                        btnBorderColor='#52C8B2'
+                                        btnBackgroundColor='#52C8B2'
+                                        fontcolor='#333'
+                                        activeFontColor='#FFF'
+                                    />
+                                </View>
+                            </View>
+                            <View style={styles.popbtnbig}>
+                                <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>취소</Text></TouchableOpacity>
+                                <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>등록하기</Text></TouchableOpacity>
+                            </View>
                         </View>
-                    </View>
-                    <View style={styles.popbtnbig}>
-                        <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>취소</Text></TouchableOpacity>
-                        <TouchableOpacity style={styles.bigbtn} onPress={this.togglePop}><Text style={{ fontSize: 16, color: '#FFF' }}>등록하기</Text></TouchableOpacity>
                     </View>
                 </View>
             )
@@ -112,10 +115,10 @@ export default class Isbnsearch extends React.Component {
                     <Text style={{ color: '#FFF', fontSize: 20 }}>책의 ISBN을 입력하세요</Text>
                 </View>
                 <View style={styles.secondbox}>
-                    <TextInput style={styles.input} 
-                        placeholder = "Enter ISBN"
-                        onChangeText = {(text) => {this.ISBN = text}}
-                        value = {this.ISBN}
+                    <TextInput style={styles.input}
+                        placeholder="Enter ISBN"
+                        onChangeText={(text) => { this.ISBN = text }}
+                        value={this.ISBN}
                     />
                     <TouchableOpacity style={styles.searchbtn} onPress={this.togglePop}>
                         <IonIcon name="ios-search" size={30} color='#FFF' />
@@ -127,15 +130,11 @@ export default class Isbnsearch extends React.Component {
                     </TouchableOpacity>
                 </View>
                 <Modal isVisible={this.state.isPopVisible}>
-                    <View style={styles.popfirst}>
-                        <View style={styles.popsecond}>
-                            {dataDisplay}
-                        </View>
-                    </View>
+                    {dataDisplay}
                 </Modal>
             </View>
         );
-        
+
     }
 }
 const styles = StyleSheet.create({
